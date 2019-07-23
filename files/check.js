b4fbb07c152cefc36e120f83318147775cc29c58
Clément Courageux <clementcourageuxsudan@gmail.com>
2019-07-23 22:57:52 +0200

    script for truffle

--- files/check.js
+++ files/check.js
@@ -15,26 +15,29 @@ module.exports = async function(callback) {
 		
 	}).then(function(inst) {
 	    instance = inst
+	    
+	    instance.getBalance(accounts[0]).then(function(b){
+		console.log("Funds account 1: "+b.toNumber())
+	    });
 
-	    return instance.getBalance(accounts[0])
-	}).then(function(val){
-	    console.log(val.toNumber())
+	    instance.getBalance(accounts[1]).then(function(b1){
+		console.log("Funds account 2: "+b1.toNumber())
+	    });
+
+	    instance.getBalance(accounts[2]).then(function(b2){
+		console.log("Funds account 3: "+b2.toNumber())
+	    });
 	    return instance.sendCoin(accounts[1], 1, {from: accounts[0]})
-	}).then(function(t){
-	    console.log(t)
-	    return instance.getBalance(accounts[0])
 	}).then(async function(val){
-	    console.log(val.toNumber())
+	    //console.log(val.toNumber())
 	    for (let step = 0; step < 100; step++) {
 		// Runs 5 times, with values of step 0 through 4.
-		/*let t = await*/ instance.sendCoin(accounts[2], 1, {from: accounts[0]})
-		let b = await instance.getBalance(accounts[0])
-		console.log(b.toNumber())
-		/*.then(
-		   
-		   function(t){
-			instance.getBalance(accounts[0]).then(function(val){console.log(val.toNumber())})
-		    })*/
+		/*let t = await*/
+		instance.sendCoin(accounts[1], 1, {from: accounts[0]})
+		instance.sendCoin(accounts[2], 1, {from: accounts[1]})
+		instance.sendCoin(accounts[0], 1, {from: accounts[2]})
+		//let b = await instance.getBalance(accounts[0])
+		//console.log(b.toNumber())
 	    }
 	});
     
